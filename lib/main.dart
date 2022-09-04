2a59af07df4924c755951a3b1d4e0b2f9983d98c
Bintang Yoga Pamungkas <bintang.pamungkas@stockbit.com>
2022-09-04 08:00:07 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:airplane/ui/pages/get_started_page.dart';
+import 'package:airplane/ui/pages/sign_up_page.dart';
+import 'package:airplane/ui/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+// import 'package:airplane/ui/pages/get_started_page.dart';
+// import 'ui/pages/splash_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/get-started': (context) => GetStartedPage(),
+        '/sign-up': (context) => SignUpPage(),
+      },
+    );
+  }
+}
